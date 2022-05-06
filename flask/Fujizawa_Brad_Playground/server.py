65de6e85ccd1d1e2d714c79348ea708d6203c901
Brad Fujizawa <bafujizawa@gmail.com>
2022-05-06 08:04:17 -0700

    modified

--- flask/Fujizawa_Brad_Playground/server.py
+++ flask/Fujizawa_Brad_Playground/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return 'Go to play/boxes'
+
 @app.route('/play/<int:boxes>')
 def play(boxes):
-    return render_template('play.html', boxes = boxes)
+    return render_template('index.html', boxes = boxes)
 
 if __name__ == '__main__':
     app.run(debug=True)
